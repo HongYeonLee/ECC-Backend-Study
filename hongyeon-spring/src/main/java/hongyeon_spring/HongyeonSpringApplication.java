bb7e8a53f50524afa9504f073c248faaf45ee0f3
HongYeonLee <llhongyan@naver.com>
2026-05-03 01:19:23 +0900

    스프링 프로젝트 초기 설정

---
+++ hongyeon-spring/src/main/java/hongyeon_spring/HongyeonSpringApplication.java
@@ -0,0 +1,13 @@
+package hongyeon_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HongyeonSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HongyeonSpringApplication.class, args);
+	}
+
+}
---
+++ hongyeon-spring/src/test/java/hongyeon_spring/HongyeonSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package hongyeon_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HongyeonSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
