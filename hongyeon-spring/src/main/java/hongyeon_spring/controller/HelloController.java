9a0f979419ecb93ecde2ef67ab6bc196e884f31f
HongYeonLee <llhongyan@naver.com>
2026-05-03 16:03:03 +0900

    동적 콘텐츠 구현하기

--- hongyeon-spring/src/main/java/hongyeon_spring/controller/HelloController.java
+++ hongyeon-spring/src/main/java/hongyeon_spring/controller/HelloController.java
@@ -3,13 +3,21 @@ package hongyeon_spring.controller;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
     @GetMapping("hello") // 웹 브라우저에 /hello가 들어오면 밑의 함수를 실행
     public String hello(Model model) {
-        model.addAttribute("data", "hello!!");
+        model.addAttribute("data", "hello!!"); //키 data, 값 hello!!
         return "hello"; // resources/templates/hello.html 렌더링 해라. 모델을 넘기면서
+    }
+
+    @GetMapping("hello-mvc")
+    //입력값 name을 받음
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name); //키 name, 값 name
+        return "hello-template";
     }
 }
