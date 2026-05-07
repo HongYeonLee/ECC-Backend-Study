2fed914d18c45735ca0aa2edc37abe83d5cddfec
HongYeonLee <llhongyan@naver.com>
2026-05-07 10:59:52 +0200

    API 방식 구현하기

--- hongyeon-spring/src/main/java/hongyeon_spring/controller/HelloController.java
+++ hongyeon-spring/src/main/java/hongyeon_spring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -16,8 +17,37 @@ public class HelloController {
 
     @GetMapping("hello-mvc")
     //입력값 name을 받음
-    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+    public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name); //키 name, 값 name
         return "hello-template";
+    }
+    
+    @GetMapping("hello-string")
+    @ResponseBody //http 프로토콜에서 header부와 body부가 있을 때, body부에 리턴값을 직접 넣어주겠다는 뜻
+    public String helloString(@RequestParam("name") String name) {
+    	return "hello " + name; // "hello string", 뷰가 없고 클라이언트에게 바로 해당 문자가 내려감
+    }
+    
+    @GetMapping("hello-api")
+    @ResponseBody
+    
+    public Hello helloApi(@RequestParam("name") String name) {
+    	Hello hello = new Hello(); //객체 생성
+    	hello.setName(name);
+    	return hello; //객체를 넘겨줌
+    }
+    
+    static class Hello { //static 쓰면 클래스 안에 또 클래스 만들 수 있음 사용은 상위클래스명.하위클래스명
+    	private String name;
+
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+    	
+    	
     }
 }
